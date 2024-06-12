df57ad0b7d4598321305e81597efa8df6ee04b61
AKASH <87299311+gamkers@users.noreply.github.com>
2024-06-12 14:52:58 +0530

    Update save_location.php

--- save_location.php
+++ save_location.php
@@ -1,1 +1,11 @@
+<?php
+// Get the location data from the POST request
+$locationData = file_get_contents('php://input');
 
+// Write the location data to "location.json"
+file_put_contents('location.json', $locationData);
+
+// Optionally, you can send a response back to the client
+http_response_code(200);
+echo "Location data saved successfully.";
+?>
